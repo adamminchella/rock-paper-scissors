5a901ae112e2d84a8ce0b6bf438b269a4d1446e5
Adam Minchella <adamminchella@msn.com>
2022-04-08 19:10:12 +0100

    Add innerHTML remarks and restart button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const restart = document.querySelector(".restart");
+let playerScore = document.querySelector(".player__score");
+let computerScore = document.querySelector(".computer__score");
+let remark = document.querySelector(".remark");
+playerScore.innerHTML = 0;
+computerScore.innerHTML = 0;
+
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
@@ -21,19 +31,27 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
-  } else if (
+    playerScore.innerHTML++;
+    if (playerScore.innerHTML === "5") {
+      return (remark.innerHTML = "Congrats, you've fucked the pc");
+    }
+    remark.innerHTML = `You win! ${playerSelection} beats ${computerSelection}!`;
+    return;
+  }
+  if (
     (playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Rock")
   ) {
-    return `You lose! ${computerSelection} beats ${playerSelection}!`;
-  } else "It's a tie!";
+    computerScore.innerHTML++;
+    if (computerScore.innerHTML === "5") {
+      return (remark.innerHTML = "Oh dear, you've been fucked");
+    }
+    remark.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    return;
+  }
+  remark.innerHTML = "It's a tie!";
 }
-
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
 
 rock.addEventListener("click", playRound);
 paper.addEventListener("click", playRound);
@@ -49,4 +67,12 @@ function findPlayerSelection() {
   if (rock.classList.contains("scissors")) {
     return (playerSelection = "Scissors");
   }
+}
+
+restart.addEventListener("click", restartGame);
+
+function restartGame() {
+  remark.innerHTML = "";
+  playerScore.innerHTML = 0;
+  computerScore.innerHTML = 0;
 }
